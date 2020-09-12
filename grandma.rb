cf880414652b9f6f53653c6205ddb97215e62124
conner fortner <fortner.conner@yahoo.com>
2020-09-12 04:36:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,3 +14,14 @@
 
 
 
+def speak_to_grandma(phrase)
+    if phrase == 'I LOVE YOU GRANDMA!'
+     return 'I LOVE YOU TOO PUMPKIN!'
+    elsif phrase != phrase.upcase
+     return 'HUH?! SPEAK UP, SONNY!'
+    elsif phrase == phrase.upcase
+      # && phrase != 'I LOVE YOU GRANDMA!'
+     return 'NO, NOT SINCE 1938!'
+    end
+end
+puts speak_to_grandma("I LOVE YOU GRANDMA!")
